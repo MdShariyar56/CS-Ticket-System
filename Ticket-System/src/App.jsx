b87c10e4b6804c272f47f7f4c15ca0b49ce505f0
MdShariyar56 <shahriyarkobir2005@gmail.com>
2025-09-25 00:07:53 +0600

    Added a navber

--- Ticket-System/src/App.jsx
+++ Ticket-System/src/App.jsx
@@ -5,50 +5,34 @@ const App = () => {
     
     
       <>
-{/* <div className="navbar d-flex justify-center items-center bg-base-100 shadow-sm max-w-[1200px] max-auto">
-  <div className=" ">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+
+        <div class="navbar bg-base-100 shadow-sm flex justify-around items-center">
+          <a class="btn btn-ghost text-xl ml-8">CS — Ticket System</a>
+  {/* বাম পাশের অংশ: লোগো এবং মোবাইলের জন্য টোগল মেনু */}
+  <div>
+    <div class="dropdown">
+      <div tabIndex={0} role="button" class="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
       </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+      <ul tabIndex={0} class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li><a>Link</a></li>
+        <li><a>Home</a></li>
+        <li><a>FAQ</a></li>
+        <li><a>Changelog</a></li>
+        <li><a>Blog</a></li>
+        <li><a>Download</a></li>
+        <li><a>Contact</a></li>
+        <li><a class="btn btn-primary">+ New Ticket</a></li>
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    
   </div>
-  
-  <div className="">
-    <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Home</a></li>
-      <li><a>FAQ</a></li>
-      <li><a>Changelog</a></li>
-      <li><a>Blog</a></li>
-      <li><a>Download</a></li>
-      <li><a>Contact</a></li>
-    </ul>
-  </div>
-    <a className="btn btn-primary">+ New Ticket</a>
-  </div>
-</div> */}
 
-<div class="navbar bg-base-100 shadow-sm flex justify-around items-center">
-  <div>
-    <a class="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div>
-    <ul class="menu menu-horizontal px-1">
+  {/* ডান পাশের অংশ: ডেস্কটপের জন্য সম্পূর্ণ মেনু */}
+  <div class="hidden lg:flex">
+    <ul class="menu menu-horizontal px-10">
       <li><a>Link</a></li>
       <li><a>Home</a></li>
       <li><a>FAQ</a></li>
@@ -56,8 +40,8 @@ const App = () => {
       <li><a>Blog</a></li>
       <li><a>Download</a></li>
       <li><a>Contact</a></li>
-      <li><a className="btn btn-primary ">+ New Ticket</a></li>
     </ul>
+    <a class="btn btn-primary">+ New Ticket</a>
   </div>
 </div>
 
